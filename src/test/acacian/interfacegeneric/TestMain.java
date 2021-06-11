37794338fee053189f4135e7854f788f72b97557
DESKTOP-1FENOAP\ACACIAN_L14 <gkehdrn95@naver.com>
2021-06-11 19:04:14 +0900

    스트림 연습

--- src/test/acacian/interfacegeneric/TestMain.java
+++ src/test/acacian/interfacegeneric/TestMain.java
@@ -1,11 +1,18 @@
 package test.acacian.interfacegeneric;
 
+import java.util.stream.IntStream;
+
 public class TestMain {
 	public static void main(String[] args) {
-		ClassA<String> a = new ClassB<String>();
-		//???????????????????????????????????
-		
-		//깃플로우 테스트 테스트
-		
+		IntStream stream = IntStream.rangeClosed(1, 9);
+		stream.forEach(TestMain::loop);
+	}
+
+	static void loop(int i) {
+		IntStream stream = IntStream.rangeClosed(1, i);
+
+		String[] arr = new String[i];
+		stream.forEach(j -> arr[j - 1] = String.valueOf(j));
+		System.out.println(String.join(",", arr));
 	}
 }
