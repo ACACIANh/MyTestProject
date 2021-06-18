f2cdda56a26fe02a8913ff4766ce1ee74f574c69
DG <gkehdrn95@naver.com>
2021-06-18 20:53:06 +0900

    싱글턴 제너릭상속 연습

---
+++ src/test/acacian/singletonHeritance/MySingleton.java
@@ -0,0 +1,19 @@
+package test.acacian.singletonHeritance;
+
+public class MySingleton<T> {
+/*
+ 	//자바에서는 static 필드에서 제너릭 타입을 가질수 없다. (인스턴스 생성도)
+	protected static T instance;
+	
+	private MySingleton(){
+		
+	}
+	
+	public static Object getInstance() {
+		if(null == instance) {
+			instance = new T();
+		}	
+		return instance;
+	}
+*/
+}
